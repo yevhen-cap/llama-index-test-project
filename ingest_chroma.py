03e44008799500650be5699bca8041c5d420d9da
Yevhen Podluzhnyi <kpiwelder@gmail.com>
2025-10-07 16:34:21 +0300

    feat: steamlit chat
    Azure OpenAI based
    
    chore: updated gitignore

---
+++ ingest_chroma.py
@@ -0,0 +1,61 @@
+import pathlib as pl
+from os import getenv
+
+import chromadb
+from llama_index.core import (Settings, SimpleDirectoryReader, StorageContext,
+                              VectorStoreIndex)
+from llama_index.core.node_parser import SentenceSplitter, SimpleNodeParser
+from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from loguru import logger
+from llama_index.llms.azure_openai import AzureOpenAI
+
+
+INDEX_NAME = getenv("INDEX_NAME")
+
+chroma_client = chromadb.PersistentClient(f"./db/{INDEX_NAME}")
+llm = AzureOpenAI(
+    engine=getenv("EMBED_MODEL_NAME"),
+    model=getenv("EMBED_MODEL_NAME"),
+    api_version="2024-02-01",
+)
+
+def create_chroma_index() -> None:
+    if not pl.Path(f"./db/{INDEX_NAME}").exists():
+        logger.info(f"Creating index: {INDEX_NAME}")
+        chroma_client.get_or_create_collection("new-collection")
+
+
+def ingest_documents() -> None:
+    logger.info("Read Directory with files")
+    reader = SimpleDirectoryReader(
+        "./data/data/TEACHER", recursive=True, required_exts=[".pdf"]
+    )
+    logger.info("Load data")
+    documents = reader.load_data()
+    np: SimpleNodeParser = SimpleNodeParser.from_defaults(
+        chunk_size=512, chunk_overlap=20
+    )
+
+    logger.info("Creating nodes")
+    np.get_nodes_from_documents(documents)
+
+    Settings.llm = llm
+    Settings.embed_model = AzureOpenAIEmbedding(model=getenv("EMBED_MODEL_NAME"))
+    Settings.node_parser = SentenceSplitter(chunk_size=512, chunk_overlap=20)
+    logger.info("Configuring ChromaDB index")
+    collection = chroma_client.get_or_create_collection("new-collection")
+    vector_store = ChromaVectorStore(chroma_collection=collection)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    logger.info("Creating index from documents")
+    VectorStoreIndex.from_documents(
+        documents=documents,
+        storage_context=storage_context,
+        show_progress=True,
+    )
+
+
+if __name__ == "__main__":
+    create_chroma_index()
+    ingest_documents()
---
+++ main.py
@@ -0,0 +1,86 @@
+from os import getenv
+from typing import Union
+
+import chromadb
+import streamlit as st
+from llama_index.core import VectorStoreIndex
+from llama_index.core.chat_engine.types import ChatMode
+from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
+from llama_index.llms.azure_openai import AzureOpenAI
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from loguru import logger
+
+INDEX_NAME = getenv("INDEX_NAME", "test-index")
+
+llm = AzureOpenAI(
+    engine=getenv("MODEL_NAME"),
+    model=getenv("MODEL_NAME"),
+    temperature=0.0,
+    api_version="2024-12-01-preview"
+)
+
+
+def get_chroma_index_vector_store() -> VectorStoreIndex:
+    chroma_client = chromadb.PersistentClient(f"./db/{INDEX_NAME}")
+    chroma_collection = chroma_client.get_or_create_collection("new-collection")
+    chroma_vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+    vector_store: VectorStoreIndex = VectorStoreIndex.from_vector_store(
+        vector_store=chroma_vector_store,
+        embed_model=AzureOpenAIEmbedding(model=getenv("EMBED_MODEL_NAME"))
+    )
+
+    return vector_store
+
+
+def start_chat(index: VectorStoreIndex):
+    if "chat_engine" not in st.session_state.keys():
+        st.session_state.chat_engine = index.as_chat_engine(
+            chat_mode=ChatMode.CONTEXT,
+            verbose=True,
+            llm=llm
+        )
+
+    st.set_page_config(
+        page_title="Project-X chatting tool",
+        page_icon="🤦‍♂️",
+        layout="centered",
+        initial_sidebar_state="auto",
+        menu_items=None,
+    )
+
+    st.title("Test project chatting tool 🤦‍♂️")
+
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [
+            {
+                "role": "assistant",
+                "content": "Ask me about these templates?",
+            }
+        ]
+
+    if prompt := st.chat_input("Your question"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = st.session_state.chat_engine.chat(message=prompt)
+                st.write(response.response)
+                message = {"role": "assistant", "content": response.response}
+                st.session_state.messages.append(message)
+
+
+def main() -> None:
+    index = None
+    logger.info("Running ChromaDB")
+    index = get_chroma_index_vector_store()
+    start_chat(index)
+
+
+if __name__ == "__main__":
+    main()
